7c5c8737a977455c5d25d307c85bd9de342b455d
David Wen <dstwen@gmail.com>
2015-04-28 22:08:56 -0700

    add table for texts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429030017) do
+ActiveRecord::Schema.define(version: 20150429050702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150429030017) do
   create_table "dictionaries", force: true do |t|
     t.string   "word"
     t.string   "translation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "texts", force: true do |t|
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
